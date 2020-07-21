0fa49f8a1d035d5372b642820bcf30282c9825ed
JustJal <jalinson5@gmail.com>
2020-07-21 14:37:53 -0400

    Pre-Alpha-0.0.2.1 -MInor documentation added. -Table.txt  added. -Minor changes on Readme.md

--- DataAccess/ConnectionToSQL.cs
+++ DataAccess/ConnectionToSQL.cs
@@ -11,7 +11,7 @@ namespace DataAccess
     {
         private readonly string connectionString;
         public ConnectionToSQL()
-        {
+        {       /* You must change this code for using on another SQL Server DataBase, change only the "server" and "DataBase" strings and be sure to read the Table.txt*/
             connectionString = @"Server=ANA\SQLEXPRESS01;DataBase=Employees; integrated security = true";
         }
         protected SqlConnection GetConnection()
